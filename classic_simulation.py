0c19fdb561ed6e29c418c773e19287de42725156
Miguel Ruiz <miguelruiz98@outlook.com>
2021-09-02 22:23:52 -0500

    random walks over genotype networks

---
+++ classic_simulation.py
@@ -0,0 +1,95 @@
+import numpy as np
+import networkx as nx
+from scipy.stats import expon
+from ibmcloudant.cloudant_v1 import CloudantV1, Document
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+import pandas as pd
+import os
+import time
+
+def infinitesimalGenerator(G, gamma):
+  # gamma is the mutation rate 
+
+  A = nx.laplacian_matrix(G)
+  H = -gamma* A
+
+  return H
+
+def nextState(states, i, Q):
+  # choose next state to jump to from the i-th row of the jump process transition matrix
+  # states are the set of state of the random walk
+  # i is the actual state
+  # Q is the infinitesimal generator of the continuous random walk
+  probs = [-Q[i, j]/Q[i, i] for j in states]
+  probs[i] = 0 
+
+  return np.random.choice(states, p = probs)
+
+def simulation_cw(gspace, gspace_name, phenotypes, initial_genotype, max_simulation_time, gamma):
+  start = time.time()
+
+  tau = {} # tau (hitting time) for every phenotype: time it takes quantum walk to find given phenotype
+  N = {} # number of jumps the random walk takes to find a genotype with a new phenotype
+
+  H = infinitesimalGenerator(gspace, gamma)
+
+  jump = 0
+  time = 0
+  
+  #initialization
+  for phen in phenotypes:
+    tau[phen] = -1
+    N[phen] = -1
+
+  actual_state = initial_genotype 
+
+  # phenotypes of actual state
+  phenotypes_actual_state = gspace.nodes[actual_state]['phenotypeName']
+
+  # start of simulation
+  while time < max_simulation_time:
+    for phen in phenotypes_actual_state:
+      if tau[phen] < 0: # update hitting times of novel phenotypes
+        tau[phen] = time
+        N[phen] = jump
+
+    T = expon.rvs(scale = -1/H[actual_state, actual_state], size=1)[0] # holding time
+
+    time += T
+    jump += 1
+    
+    # choice next random state
+    actual_state = nextState(list(gspace.nodes), actual_state, H)
+
+    # phenotypes of actual state
+    phenotypes_actual_state = gspace.nodes[actual_state]['phenotypeName']
+  
+  # end of simulation
+  end = time.time()
+
+  # database connection
+  authenticator = IAMAuthenticator(os.environ['CLOUDANT_APIKEY'])
+
+  cloudant = CloudantV1(authenticator=authenticator)
+  cloudant.set_service_url(os.environ['CLOUDANT_URL'])
+
+  client = cloudant.new_instance()
+
+  simulation: Document = Document()
+
+  simulation.initial_gen_index = initial_genotype
+  simulation.initial_gen = gspace.nodes[initial_genotype]['sequence']
+  simulation.initial_phen = gspace.nodes[initial_genotype]['phenotypeName'][0]
+  simulation.transition_rate = gamma
+  simulation.max_simulation_time = max_simulation_time
+  simulation.total_mutations = jump
+  simulation.computing_time = end-start
+
+  for phen in phenotypes:
+    setattr(simulation, 'tau_'+phen, tau[phen] if tau[phen] >= 0 else time)
+    setattr(simulation, 'mutations_'+phen, N[phen])
+    
+  client.post_document(
+    db="simulations-cw-"+gspace_name,
+    document=simulation
+  )
---
+++ main.py
@@ -0,0 +1,29 @@
+import classic_simulation
+import json
+import os
+import networkx as nx
+
+if __name__ == "__main__":
+    
+    data_file  = open("data.json", "r")
+    data = json.load(data_file)
+    data_file.close()
+
+    # parameters
+    names = ['four_phen', 'musculus', 'neurospora', 'arabidopsis']
+    gspace_name = names[int(os.environ['GSPACE'])]
+    gspace = nx.read_gml(data[gspace_name]['g-space'], label='id')
+
+    phenotypes_file = open(data[gspace_name]["phenotypes"], "r")
+    phenotypes = phenotypes_file.read().splitlines()
+    phenotypes_file.close()
+
+    genotype_networks_file = open(data[gspace_name]["g-networks"], "r")
+    genotype_networks = json.load(genotype_networks_file)
+    genotype_networks_file.close()
+
+    initial_genotype = 200
+    max_simulation_time = 1000000000
+    gamma_c = 10**(-5)
+    
+    classic_simulation.simulation_cw(gspace, gspace_name, phenotypes, initial_genotype, max_simulation_time, gamma_c)
